4b4a266a8a670ca700d935a4677624599023a790
np-viet-anh <anh.nguyenviet3110@hcmut.edu.vn>
2025-04-13 03:13:19 +0700

    Anova

---
+++ analysis_anova.R
@@ -0,0 +1,29 @@
+library(car)       # để dùng leveneTest
+library(dplyr)     # để xử lý dữ liệu
+
+# 1. Chuẩn bị dữ liệu
+GPU <- read.csv("data/All_GPUs.csv", stringsAsFactors = FALSE)
+GPU[GPU == ""] <- NA
+GPU$Memory_Bandwidth <- as.numeric(gsub(" GB/s", "", GPU$Memory_Bandwidth))
+
+GPU <- GPU[!is.na(GPU$Memory_Bandwidth), ]
+
+# 2. Phân nhóm hãng
+AMD <- subset(GPU, Manufacturer == "AMD")
+Nvidia <- subset(GPU, Manufacturer == "Nvidia")
+
+# 3. Kiểm tra phân phối chuẩn
+qnorm(AMD$Memory_Bandwidth)
+qqline(AMD$Memory_Bandwidth, col="red")
+shapiro.test(AMD$Memory_Bandwidth)     # -> p < 0.05
+
+qqnorm(Nvidia$Memory_Bandwidth)
+qqline(Nvidia$Memory_Bandwidth, col="red")
+shapiro.test(Nvidia$Memory_Bandwidth)  # -> p < 0.05
+
+# 4. Kiểm định đồng nhất phương sai
+leveneTest(Memory_Bandwidth ~ as.factor(Manufacturer), data = GPU)
+
+# 5. Phân tích phương sai (ANOVA)
+anova_model <- aov(Memory_Bandwidth ~ Manufacturer, data = GPU)
+summary(anova_model)
