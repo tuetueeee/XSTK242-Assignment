2a3975ab2f2debd5e72a5cb761eeba26d78fa7a6
np-viet-anh <anh.nguyenviet3110@hcmut.edu.vn>
2025-04-18 02:32:52 +0700

    Phuong sai

--- analysis_anova.R
+++ analysis_anova.R
@@ -1,16 +1,38 @@
 library(car)       # để dùng leveneTest
 library(dplyr)     # để xử lý dữ liệu
 
-# 1. Chuẩn bị dữ liệu
-GPU <- read.csv("D:/242_XSTK/XSTK242-Assignment/All_GPUs.csv", stringsAsFactors = FALSE)
-GPU[GPU == ""] <- NA
-GPU$Memory_Bandwidth <- as.numeric(gsub(" GB/s", "", GPU$Memory_Bandwidth))
+# Doc du lieu
+All_GPUs <- read.csv("~/HCMUT/242 XSTK/btl/archive/All_GPUs.csv")
+head(All_GPUs)
 
-GPU <- GPU[!is.na(GPU$Memory_Bandwidth), ]
+All_GPUs[All_GPUs == ""] <- NA
+All_GPUs[] <- lapply(All_GPUs, function(x) gsub ("^\\n- $", NA, x))
+All_GPUs[] <- lapply(All_GPUs, function(x) gsub ("^\\ n$", NA, x))
+
+library(questionr)
+freq.na(All_GPUs)
+
+NA_summary <- data.frame(freq.na(All_GPUs))
+colnames(NA_summary) <- c("Missing", "Percent")
+selected_columns <- rownames(NA_summary[NA_summary$Percent < 10, ])
+new_GPU_data <- All_GPUs[, selected_columns]
+new_GPU_data <- na.omit(new_GPU_data)
+
+str(new_GPU_data)
+
+columns_to_clean <- c("L2_Cache","Memory_Bandwidth","Memory_Bus","Memory_Speed")
+
+remove_units <- function(column) {
+  cleaned_column <- gsub("[^0-9.]", "", column)
+  cleaned_column <- as.numeric(cleaned_column)
+  return(cleaned_column)
+}
+
+new_GPU_data[columns_to_clean] <- lapply(new_GPU_data[columns_to_clean], remove_units)
 
 # 2. Phân nhóm hãng
-AMD <- subset(GPU, Manufacturer == "AMD")
-Nvidia <- subset(GPU, Manufacturer == "Nvidia")
+AMD <- subset(new_GPU_data, Manufacturer == "AMD")
+Nvidia <- subset(new_GPU_data, Manufacturer == "Nvidia")
 
 # 3. Kiểm tra phân phối chuẩn
 qnorm(AMD$Memory_Bandwidth)
@@ -22,8 +44,9 @@ qqline(Nvidia$Memory_Bandwidth, col="red")
 shapiro.test(Nvidia$Memory_Bandwidth)  # -> p < 0.05
 
 # 4. Kiểm định đồng nhất phương sai
-leveneTest(Memory_Bandwidth ~ as.factor(Manufacturer), data = GPU)
+leveneTest(new_GPU_data$Memory_Bandwidth ~ as.factor(new_GPU_data$Manufacturer), data = new_GPU_data)
+# -> p < 0.05, không đồng nhất phương sai
 
 # 5. Phân tích phương sai (ANOVA)
-anova_model <- aov(Memory_Bandwidth ~ Manufacturer, data = GPU)
+anova_model <- aov(Memory_Bandwidth ~ Manufacturer, data = new_GPU_data)
 summary(anova_model)
