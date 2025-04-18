5fadb5a8bb1ce3df042ba99750f42ca1c4945bfa
unknown <tung.nguyenquang2005@hcmut.edu.vn>
2025-04-18 16:41:32 +0700

    Your message describing the changes

--- Xu_Ly_So_Lieu.R
+++ Xu_Ly_So_Lieu.R
@@ -1,20 +1,44 @@
 library(dplyr)
+
+
 GPUs_list <- read.csv("D:/CS_Major/Đại Cương/XSTK_BTL/All_GPUs.csv", na.strings = c("", "N/A"))
+
+GPUs_list[GPUs_list == ""] <- NA
+GPUs_list[] <- lapply(GPUs_list, function(x) gsub("^\\n- $", NA, x))
+GPUs_list[] <- lapply(GPUs_list, function(x) gsub("^\\n$", NA, x))
 
 missing_percent <- sapply(GPUs_list, function(x) sum(is.na(x)) / length(x) * 100)
 missing_percent <- sort(missing_percent)
 
-selected_cols <- names(missing_percent[missing_percent < 10])[1:10]
+selected_cols <- names(missing_percent[missing_percent < 10])
 
-df_gpu <- GPUs_list[, selected_cols]
+important_cols <- c("Memory_Speed", "L2_Cache", "Dedicated", "Memory_Bus", "Shader", "Memory_Bandwidth")
+df_gpu <- GPUs_list[, important_cols]
 
-clean_numeric <- function(x){
+clean_numeric <- function(x) {
   as.numeric(gsub("[^0-9\\.]", "", x))
 }
 
-numeric_like_patterns <- c("Shader", "Speed", "Bus", "Bandwidth", "Open_GL", "Direct_X")
-numeric_cols <- names(df_gpu)[sapply(numeric_like_patterns, function(p) grepl(paste(p, collapse="|"), names(df_gpu))) %>% apply(2, any)]
+df_gpu <- df_gpu %>%
+  rename(
+    `Memory_Speed (MHz)` = Memory_Speed,
+    `Memory_Bus (bit)` = Memory_Bus,
+    `Memory_Bandwidth (GB/s)` = Memory_Bandwidth,
+    `L2_Cache (MB)` = L2_Cache,
+    `Dedicated (GB)` = Dedicated
+  )
 
-df_gpu[numeric_cols] <- lapply(df_gpu[numeric_cols], clean_numeric)
 
-GPUs_final <- df_gpu
+df_gpu <- df_gpu %>%
+  mutate(
+    `Memory_Speed (MHz)` = clean_numeric(`Memory_Speed (MHz)`),
+    `L2_Cache (MB)` = clean_numeric(`L2_Cache (MB)`),
+    `Dedicated (GB)` = clean_numeric(`Dedicated (GB)`),
+    `Memory_Bus (bit)` = clean_numeric(`Memory_Bus (bit)`),
+    Shader = clean_numeric(Shader),
+    `Memory_Bandwidth (GB/s)` = clean_numeric(`Memory_Bandwidth (GB/s)`)
+  )
+
+GPUs_final <- df_gpu %>% 
+  filter(!is.na(`Memory_Bandwidth (GB/s)`))
+
